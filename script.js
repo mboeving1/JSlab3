282004476c213b0d62e62dde81a653e1e2bb5c60
Matthew Boeving <mboeving1@gmail.com>
2021-06-22 20:59:18 -0400

    Second attempt at JSlab-3

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ function addSubmission(submissions, newName, newScore, newDate) {
   submissions.push(submission);
 }
 
-addSubmission(submissions, "Scott", 99, "2021-01-01"); //
+// addSubmission(submissions, "Scott", 99, "2021-01-01"); //
 
-console.log(submissions);
+// console.log(submissions);
 
 // Declare a function named deleteSubmissionByIndex
 // parameters: array, index
@@ -75,9 +75,7 @@ function deleteSubmissionByName(_array, name) {
 
 //deleteSubmissionByName(submissions, "Joe");
 
-//console.log(submissions);
-
-
+console.log(submissions);
 
 //Declare a function named editSubmission
 //parameters: array, index, score
@@ -86,91 +84,70 @@ function deleteSubmissionByName(_array, name) {
 //or equal to 60 and false otherwise.
 
 function editSubmission(array, index, score) {
-  var index = submissions.findIndex((element) => element.score === newScore);
-  
-submissions[index].score = newScore;
+  submissions[index].score = score;
+  submissions[index].passed = score >= 60;
 }
 
-editSubmission(submissions, 1, 1);////   Isn't working....
-// My thinking was that I would create the variable index and have it equal
-//new score. Use that variable as a way to transform the score at the 
-//particular index into newScore.
-
-
-
+editSubmission(submissions, 1, 1); //will edit Joe's score to 1
 
 //Declare a function named findSubmissionByName
 //Parameter(s): array, name
 //Functionality: return the object in the array that has the provided name. Use the find method
 
-function findSubmissionByName(array, name){
-  let n = console.log(submissions[1]);
-  var object = submissions.find((element) => element.givenName === n);
+function findSubmissionByName(array, name) {
+  var object = submissions.find((element) => element.name === name);
 
-     return (object,name);//only works by changing index number for n.
-  }
+  console.log(object);
+}
 
-  // findSubmissionByName(submissions, "Joe")//should return Joe object
-
-
-
+findSubmissionByName(submissions, "Jack");//should return Jack object
 
 // Declare a function named findLowestScore
 // Parameter(s): array
 // Functionality: return the object in the array that has the lowest score. Use the forEach method to loop through the whole array.
 
-// //function findLowestScore(array) {
-//   let max_value = 100;
-//   let min_value = 0;
-//   var objects = submissions.forEach((object) => {
+function findLowestScore(array) {
+  var min_value = array[0];
+  
+  array.forEach((element) => {
+    if (element < min_value) {
+      min_value = element
+  }console.log(min_value);
+});
+}
 
-//I must've deleted what I had out of frustration. I know I had more for 
-//this one. I can't begin to tell you where I was going with it :(
-
-
-
+  
 //Declare a function named findAverageScore
 // Parameter(s): array
 // Functionality: return the average quiz score.  Use a for...of loop.
 
+function findAverageScore(array) {
+  let total = 0;
 
-// function findAverageScore(array){
-  
-//   let values =Object.values((array) => array.score === score);
-//   for (let value of values){
-//     console.log( value/5);
-//   }
-// }
-// findAverageScore(submissions);
-
-//I'm not sure what's wrong with this. To me it makes sense but obviously
-//I'm missing something. I was thinking to have values equal all the values
-//and divide by the number of objects in the array.
-
-
-
+  for (let i of array) {
+    total = total + i.score;
+  }
+  console.log(total / array.length);
+}
+findAverageScore(submissions);
 
 
 
 //Declare a function named filterPassing
 //Parameter(s): array
-//Functionality: return a new array using the filter method. The filter method should find objects in the array that have passing scores. 
+//Functionality: return a new array using the filter method. The filter method should find objects in the array that have passing scores.
 
-function filterPassing(array){
-
-  const pass = submissions.filter(submissions =>submissions.score >=60);
+function filterPassing(array) {
+  const pass = submissions.filter((submissions) => submissions.score >= 60);
   console.log(pass);
 }
-//filterPassing(submissions);
-
+filterPassing(submissions);
 
 // Declare a function named filter90AndAbove
 // Parameter(s): array
 // Functionality: return a new array using the filter method. The filter method should find objects in the array that have scores greater than or equal to 90.
 
-function filter90AndAbove(array){
-
-  const pass = submissions.filter(submissions =>submissions.score >=90);
+function filter90AndAbove(array) {
+  const pass = submissions.filter((submissions) => submissions.score >= 90);
   console.log(pass);
 }
-filter90AndAbove(submissions);
